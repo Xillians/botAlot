92a322546ca0195103c32a06a9f41a37f021c84c
Xillians <chkxillians@gmail.com>
2019-10-23 19:42:34 +0100

    feat: Bot goes live. feat: Responds to messages containing "alot" with a picture of an alot along with message "HELLO!"

---
+++ App.js
@@ -0,0 +1,20 @@
+var Discord = require('discord.js');
+var dotenv = require('dotenv');
+var client = new Discord.Client();
+var http = require("http");
+
+http.createServer().listen(process.env.PORT || 5000);
+dotenv.config();
+
+client.on('ready', () => {
+    console.log("Connected!");    
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (message) => {
+    if (message.content.toUpperCase().includes('ALOT')) {
+        message.channel.send("HELLO!", {files: ["https://i.imgur.com/45L4Ira.png"]});
+    }
+})
+
+client.login(process.env.BOT_TOKEN);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# BotAlot
+
+## Features
+This bot will respond if a message contains the word "alot". The response will be a picture of an alot saying "HELLO!" to chat. Enjoy it!
+
+### Running locally
+you must have node installed to run this bot.
+
+
+To just run the bot, write **npm run start**.
